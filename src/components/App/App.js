3f633948300738d3c8cef28d72ae97dfd3540e81
ngoune_cedric <gabyngoune@yahoo.fr>
2020-09-01 12:31:15 +0200

    Modification du fichier App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { authEndpoint, scopes, clientId, redirectUri } from "../Config/config";
-import Player from "../Player/player";
+import ReactHowler from "react-howler";
 import hash from "../hash";
 import Header from "../Header/header";
 import "./App.css";
@@ -9,77 +9,71 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [
-            {
-              url: "",
-            },
-          ],
-        },
-        name: "",
-        artists: [
-          {
-            name: "",
-          },
-        ],
-        duration_ms: 0,
-      },
-      is_playing: "Paused",
-      progress_ms: 0,
-      no_data: false,
+      token: "",
+      deviceId: "",
+      loggedIn: false,
+      error: "",
+      trackName: "Track Name",
+      artistName: "Artist Name",
+      albumName: "Album Name",
+      playing: false,
+      position: 0,
+      duration: 1,
     };
-
-    this.getCurrentPlaying = this.getCurrentPlaying.bind(this);
-    this.tick = this.tick.bind(this);
+    this.playerCheckInterval = null;
+  }
+  //Vérification du loggeIn via spotify
+  handleLogin() {
+    if (this.state.token !== "") {
+      // change the loggedIn variable, then start checking for the window.Spotify variable
+      this.setState({ loggedIn: true });
+      this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+    }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     let $token = hash.access_token;
-
     if ($token) {
-      //Je rédéfini le token via la méthode setState
       this.setState({
         token: $token,
       });
-      this.getCurrentPlaying($token);
     }
-
-    this.tick();
-  }
-  tick() {
-    if (this.state.token) {
-      this.getCurrentlyPlaying(this.state.token);
-    }
-  }
-
-  getCurrentPlaying(token) {
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
-    fetch("https://api.spotify.com/v1/me", {
-      headers,
-    })
-      .then((data) => data.json())
-      .then(
-        (response) => console.log(response)
-        // this.setState({
-        //   item: response.item,
-        //   is_playing: response.is_playing,
-        //   progress_ms: response.progress_ms,
-        //   no_data: response.no_data,
-        // })
-      )
-      .catch((err) => console.log(err));
+    const results = await fetch("https://api.spotify.com/v1/me/player", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${$token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = results.json();
+    console.log(data);
   }
 
   render() {
+    const {
+      loggedIn,
+      trackName,
+      artistName,
+      albumName,
+      error,
+      playing,
+    } = this.state;
     return (
       <div className="app">
         <Header />
+        {error && <p>Error: {error}</p>}
         <div className="app__container">
-          {!this.state.token && (
+          {this.state.token ? (
+            <div>
+              <p>Artist :{artistName} </p>
+              <p>Track: {trackName} </p>
+              <p>Album: {albumName}</p>
+              <ReactHowler
+                src="http://goldfirestudios.com/proj/howlerjs/sound.ogg"
+                playing={true}
+              />
+            </div>
+          ) : (
             <a
               className="btn btn-login"
               href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -89,14 +83,6 @@ class App extends Component {
               Login to spotify
             </a>
           )}
-          {this.state.token && !this.state.no_data && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-              progress_ms={this.state.progress_ms}
-            />
-          )}
-          {this.state.no_data && <p>Just play your song in spotify</p>}
         </div>
       </div>
     );
